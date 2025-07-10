f136b343cbb15b44d50bb231e893905b6767577b
Azwad Jahan <azwadjahan77@gmail.com>
2025-07-10 15:45:18 +0530

    Added functionality for configuration: config.c and config.h

---
+++ include/elven/config.h
@@ -0,0 +1,12 @@
+
+
+#ifndef _ELVEN_CONFIG_H
+#define _ELVEN_CONFIG_H
+
+struct elv_bar_config {
+  int bar_height;
+};
+
+struct elv_bar_config *init_config();
+
+#endif
---
+++ src/config.c
@@ -0,0 +1,8 @@
+#include "elven/config.h"
+#include <stdlib.h>
+
+struct elv_bar_config *init_config() {
+  struct elv_bar_config *config = malloc(sizeof *config);
+  config->bar_height = 20;
+  return config;
+}
